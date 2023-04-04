f6fbd4bf98e0a5f1728f3d9dceff1b5c49e0fa69
Brenda Veramendi <brendaveramendi@gmail.com>
2023-04-04 12:07:41 -0300

    config eslint

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StatusBar } from 'expo-status-bar'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { HomeScreen } from './src/screens/home/HomeScreen'
+import { ProfileScreen } from './src/screens/profile/ProfileScreen'
+const Tab = createBottomTabNavigator()
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hola Mundo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name='Home' component={HomeScreen} />
+          <Tab.Screen name='Profile' component={ProfileScreen} />
+        </Tab.Navigator>
+        <StatusBar style='auto' />
+      </NavigationContainer>
+
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// })
